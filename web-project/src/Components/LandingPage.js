fe606d800d5cc4dedbdf4b5e762a57fe0700caa5
nisanadeem909 <l201141@lhr.nu.edu.pk>
2023-04-14 21:24:37 +0500

    Only landing page first section

---
+++ web-project/src/Components/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './LandingPage.css';
+import Navbar from './Navbar'
+import HomePage from './HomePage'
+
+
+function LandingPage() {
+    return (
+        <div>
+        <Navbar/>
+        <HomePage/>
+             
+ 
+       </div>
+    );
+  }
+  
+  export default LandingPage;
+  
