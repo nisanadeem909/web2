7c68c5d34a795aeab8743deb608908704492b431
komalw2001 <komalwaseemm@gmail.com>
2023-04-16 23:00:01 +0500

    fixed index.js

--- web-project/src/index.js
+++ web-project/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Component from './Components/CompanyHomePostJob'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Component />
+    <App />
   </React.StrictMode>
 );
 
