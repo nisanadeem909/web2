87c94802ab719d5c83699f4c0444c1ae04aac407
NabeehaM <nabeeha.mudassir1@gmail.com>
2023-04-16 23:12:43 +0500

    updated version

--- web-project/src/index.js
+++ web-project/src/index.js
@@ -6,28 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import CompanyHistory from './Components/companyhistory'
 import RatingCompany from './Components/ratingcompany'
 import WorksHere from './Components/workshere'
-import ContactCompany from './contactcompany';
-import OpenVacancies from './openvacancies';
-import OpenVacanciesEditable from './openvacancies-editable';
-import ContactCompanyEditable from './contactcompany-editable';
+import ContactCompany from './Components/contactcompany';
+import OpenVacancies from './Components/openvacancies';
+import OpenVacanciesEditable from './Components/openvacancies-editable';
+import ContactCompanyEditable from './Components/contactcompany-editable';
 import CompanyHistoryEditable from './Components/companyhistory-editable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-/*root.render(
+root.render(
   <React.StrictMode>
     
     <App />
   </React.StrictMode>
-);*/
+);
 
-/*root.render(<CompanyHistory />);*/
-/*root.render(<CompanyHistoryEditable />);*/
-/*root.render(<RatingCompany />);*/
-/*root.render(<WorksHere />);*/
-/*No editable version of WorksHere */
-/*root.render(<ContactCompany />);*/
-/*root.render(<ContactCompanyEditable />);*/
-/*root.render(<OpenVacancies />);*/
-/*root.render(<OpenVacanciesEditable />);*/
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
