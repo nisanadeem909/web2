0fcc4b586ea374cf7eed34bf94e8593ddc472171
NabeehaM <nabeeha.mudassir1@gmail.com>
2023-04-28 12:27:03 +0500

    nabeeha-changed-signup-page

--- web-project/src/index.js
+++ web-project/src/index.js
@@ -8,8 +8,7 @@ import Component from './Components/UserHome'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    <App/>
+    <App />
   </React.StrictMode>
 );
 
