d1f634191f1130bde740cbc3e0ffc3bf2e11634f
NabeehaM <nabeeha.mudassir1@gmail.com>
2023-04-30 20:23:41 +0500

    i forgot to push index hehehe

--- web-project/src/index.js
+++ web-project/src/index.js
@@ -3,24 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CVViewer from './Components/CVViewer';
-import { PDFViewer } from '@react-pdf/renderer';
-import Login from './Components/signup';
+
+import Post from './Components/Post';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <PDFViewer  height={window.innerHeight} width="100%">
-    <CVViewer/>
-</PDFViewer>
-*/}
-  <App />
-    {/*<Post/>*/}
+    <App />
 
   </React.StrictMode>
- 
 );
 
 // If you want to start measuring performance in your app, pass a function
