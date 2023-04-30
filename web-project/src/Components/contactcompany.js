8f02eb7ab03574f617881c495f9a21b69283c2d6
komalw2001 <komalwaseemm@gmail.com>
2023-04-30 15:30:22 +0500

    added edit contact us modal

--- web-project/src/Components/contactcompany.js
+++ web-project/src/Components/contactcompany.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import './contactcompany.css';
 
 
 function ContactCompany() {
+
+    const [contact,setContact] = useState({'website': 'www.some-website.com','email':'someone@example.com','phone':'042-3537791'});
+
     return (
         <div id="contactcompany-box">
              <div id="contactcompany-contactus">Contact Us</div>
              <br></br>
-             <div id="contactcompany-email"><a className='contactcompany-a' href="#">www.some-website.com</a></div>
-             <div id="contactcompany-website">someone@example.com</div>
-             <div id="contactcompany-landline">042-3537791</div>
+             <div id="contactcompany-email"><a className='contactcompany-a' href={"//".concat(contact.website)}>{contact.website}</a></div>
+             <div id="contactcompany-website">{contact.email}</div>
+             <div id="contactcompany-landline">{contact.phone}</div>
        </div>
     );
   }
