43da0bd106b9dea6de41ed4a6ca41508c49a4f35
komalw2001 <komalwaseemm@gmail.com>
2023-05-10 18:37:08 +0500

    added schemas

--- web-project/backend/mongo.js
+++ web-project/backend/mongo.js
@@ -1,3 +1,4 @@
+const { Double, Decimal128 } = require('mongodb');
 const mongoose = require('mongoose');
 const mongoUrl = "mongodb+srv://nisanadeem90:jobifyproject@cluster0.fqimjde.mongodb.net/";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true 
@@ -30,6 +31,40 @@ const userSchema = new Schema({
     required: true,
     minlength: 6
   }
+  ,
+  bio:{
+    type: String
+
+  },
+
+  worksAt:{
+    type: String
+    
+  },
+
+  profilePicture:{
+    type: String
+  },
+
+  skills :{
+    type: [String],
+    
+  },
+
+  education: [{
+    school: String,
+    degree: String,
+    major: String,
+    startYear: Number,
+    endYear: Number
+  }],
+
+  experience: [{
+    company: String,
+    position: String,
+    startYear: Number,
+    endYear: Number
+  }]
 
 }, {
   timestamps: true,
@@ -38,3 +73,160 @@ const userSchema = new Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+const companySchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+  ,
+
+  companyType:{
+    type: String,
+    required: true
+  },
+
+  bio:{
+    type: String
+
+  },
+
+  profilePicture:{
+    type: String
+  },
+
+  avgRating:{
+    type: Decimal128
+  },
+
+  ratings: [{
+    username: String,
+    rating: Decimal128
+  }],
+
+  aboutUs : {
+    country: String,
+    city: String,
+    website: String,
+    startYear: Number,
+    endYear: Number,
+    text: String
+  },
+
+  contact: {
+    email: String,
+    phone: String,
+    website: String
+  }
+
+}, {
+  timestamps: true,
+});
+
+const Company = mongoose.model('Company', companySchema);
+
+module.exports = Company;
+
+const notificationSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+
+  text:{
+    type: String,
+    required: true
+  },
+
+  notificationType:{
+    type: Number,
+    required: true
+  },
+
+  notificationID :{
+    type: Number,
+    required: true,
+    unique: true
+  },
+
+  date:{
+    type: Date,
+    required: true
+  }
+
+}, {
+  timestamps: true,
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
+
+const postSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+
+  text:{
+    type: String
+
+  },
+
+  postID :{
+    type: Number,
+    required: true,
+    unique: true
+  },
+
+  date:{
+    type: Date,
+    required: true
+  },
+
+  imagePath:{
+    type:String
+  },
+
+  likedBy:{
+    type: [String]
+  },
+
+  comments:[{
+    username: String,
+    date: Date,
+    text: String
+  }],
+
+  sharedBy:{
+    type: [String]
+  }
+
+}, {
+  timestamps: true,
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
