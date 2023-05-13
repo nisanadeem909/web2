be387eae69a49746fb33d138c3fe3620f195ccc5
komalw2001 <komalwaseemm@gmail.com>
2023-05-14 00:19:21 +0500

    minor changes

--- web-project/src/App.js
+++ web-project/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './Components/Navbar'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from './Components/login.js'
+import Signup from './Components/signup.js'
 
 import LandingPage from './Components/LandingPage'
 import HomePage from './Components/HomePage'
--- web-project/src/Components/EditProfile.js
+++ web-project/src/Components/EditProfile.js
@@ -8,11 +8,19 @@ import { useLocation } from 'react-router-dom';
 import Select from 'react-select'
 import Footer from './Footer';
 import axios from 'axios'
+import { SettingsOverscanSharp } from '@material-ui/icons';
 export default function EditProfile() {
   
   const [companyList,setCompanyList] = useState([]);
 
   const [selectedCompany,setSelectedCompany] = useState(null);
+
+  const [varField,setVarField] = useState(<label>Loading</label>);
+
+  const [name,setName] = useState("");
+  const [companyType,setCompanyType] = useState("");
+  const [bio,setBio] = useState("");
+  
  
   useEffect(() => {
     axios.post("http://localhost:8000/getcompanydropdownlist").then((response) => {
@@ -27,7 +35,70 @@ export default function EditProfile() {
     .catch(function (error) {
         alert(error);
     });
-  }, [/*companyList*/]);
+  }, [companyList]);
+
+  useEffect(() => {
+    const userType = sessionStorage.getItem("userType");
+    const username = sessionStorage.getItem("sessionID");
+
+    if (userType == "user")
+    {
+          setVarField(<><label className='nisa-edit-label'>
+                          Current Working Place:  
+                        </label>
+                        <Select options={companyList}
+                        placeholder="Company you worked for"
+                        className='nisa-edit-input'
+                        onChange={(choice) => setSelectedCompany(choice)}/></>);
+
+          const param = {"user":username}
+
+          //post request to server to get profile details 
+          axios.post("http://localhost:8000/getuserprofiledetails",param).then((response) => {
+              //alert(response.data);
+              //setUser(response.data.user);
+              //alert(response.data.user);
+              setName(response.data.user.name);
+              //alert('hi');
+          })
+          .catch(function (error) {
+              alert(error);
+          });
+    }
+    else if (userType == "company")
+    {
+                        
+          const param = {"user":username}
+          //post request to server to get profile details 
+          axios.post("http://localhost:8000/getcompanyprofiledetails",param).then((response) => {
+              //alert(response.data);
+              //setUser(response.data.company);
+              //alert(response.data.company.companyType);
+              setName(response.data.company.name);
+              setCompanyType(response.data.company.companyType);
+              if (response.data.company.bio)
+                setBio(response.data.company.bio);
+              
+              //alert('hi');
+          })
+          .catch(function (error) {
+              alert(error);
+          });
+    }
+  }, []);
+
+  useEffect(() => {
+    
+    const userType = sessionStorage.getItem("userType");
+
+    if (userType == "company"){
+    setVarField(<><label className='nisa-edit-label'>
+                          Company type:  
+                        </label>
+                        <input className='nisa-edit-input' type="text" value={companyType} onChange={(event)=>{setCompanyType(event.target.value)}}/>
+                        </>);
+     }
+  }, [companyType]);
 
 
   return (
@@ -57,20 +128,14 @@ export default function EditProfile() {
                 <label className='nisa-edit-label'>
                   Name:
                 </label>
-                <input className='nisa-edit-input' type="text" />
+                <input className='nisa-edit-input' type="text" value={name} onChange={(event)=>{setName(event.target.value)}}/>
 
-                <label className='nisa-edit-label'>
-                  Current Working Place:  
-                </label>
-                <Select options={companyList}
-                placeholder="Company you worked for"
-                className='nisa-edit-input'
-                onChange={(choice) => setSelectedCompany(choice)}/>
+                {varField}
 
                 <label className='nisa-edit-label'>
                   Bio:  
                 </label>
-                <textarea className='nisa-edit-input kta-editprof' ></textarea>
+                <textarea className='nisa-edit-input kta-editprof' >{bio}</textarea>
                 
                 <div className='keditprof-btns'>
                   <button className="keditprof-savebtn">Save Changes</button>
