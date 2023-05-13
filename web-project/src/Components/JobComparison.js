d81757cd100fa1ffd266e36ffdcd9a72d731a373
NabeehaM <nabeeha.mudassir1@gmail.com>
2023-05-13 22:19:33 +0500

    merge2

--- web-project/src/Components/JobComparison.js
+++ web-project/src/Components/JobComparison.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import jobicon from './workk.png'
 import Footer from './Footer'
 
@@ -7,10 +7,65 @@ import './JobComparison.css';
 import narsun from './narsun.jpg'
 import mindstorm from './mindstorm.jpg'
 import empicon from './dummy.jpg'
-
+import axios from 'axios';
+import {useState} from 'react';
 function JobComparison(props) {
-    
-    
+    const [obj,setObj] = useState({
+        comp1:"",
+        comp2:"",
+        job1:"",
+        job2:""
+        
+    });
+    const [job1obj,setjob1] = useState([]);
+    const[job2obj,setjob2] = useState([]);
+    const [jobs,setJobs] = useState([]);
+    const [comps,setComps] = useState([]);
+    const inputRef = useRef(null);
+    const setData = async () => {
+        alert("I am in axios of job comp");
+        alert("props.jobset " + JSON.stringify(props.jobset));
+        alert("props.compset " + JSON.stringify(props.compset));
+        setJobs(props.jobset);
+        setComps(props.compset);
+        
+        
+        let newobj = {"comp1": props.compset[0].compName, "comp2":props.compset[1].compName,"job1":props.jobset[0].des,"job2":props.jobset[1].des};
+        
+        //setObj({"comp1": props.compset[0].compName, "comp2":props.compset[1].compName,"job1":props.jobset[0].des,"job2":props.jobset[1].des});
+        alert("hello");
+        alert(JSON.stringify(newobj));
+        
+        
+        let salary1,degree1,exp1,wh1,paidleaves1,location1;
+        let salary2,degree2,exp2,wh2,paidleaves2,location2;
+        axios.post('http://localhost:8000/comparejobs', newobj)
+        .then(response => {alert(JSON.stringify(response.data.job1));
+            alert(JSON.stringify(response.data.job2));
+            
+            salary1 = response.data.job1.Salary;
+            inputRef.current.value = salary1;
+            //console.log("input ref = " + inputRef.current.value);
+            salary2 = response.data.job2.Salary;
+            
+            exp1 = response.data.job1.Experience;
+            exp2 = response.data.job2.Experience;
+            wh1 = response.data.job1.WeeklyWorkingHours;
+            wh2 = response.data.job2.WeeklyWorkingHours;
+            paidleaves1 = response.data.job1.YearlyPaidLeaves;
+            paidleaves2 = response.data.job2.YearlyPaidLeaves;
+
+           degree1 = response.data.job1.DegreeRequired;
+           degree2 = response.data.job2.DegreeRequired;
+            });
+        
+        //alert("Showing salary = " + job1details);
+        
+        
+      };
+    useEffect(() => {
+        setData();
+      }, []);
     
     return (
 
@@ -46,7 +101,7 @@ function JobComparison(props) {
                     
                 </tr>
                 <tr>
-                    <td>$7k Yearly + $1K Yearly Bonus</td>
+                    <td ref={inputRef}></td>
                     <td>$9k Yearly + $0.5K Yearly Bonus</td>
                     
                 </tr>
